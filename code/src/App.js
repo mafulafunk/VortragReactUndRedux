a621d5981aa11bc35f26c4ae343794aff55ad94d
Martin Funk <Martin@MartinFunk.de>
2017-02-24 15:30:48 +0100

    wip

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,21 @@
 import React, {Component} from "react";
+import HelloWorld from "./HelloWorld";
+import User from "./User";
+import UserList from "./UserList";
+import UserCreation from "./UserCreation";
 
 export default class App extends Component {
+
+    render() {
+        const user1 = {firstName: "Anton", lastName: "Hinlegen"};
+        const user2 = {firstName: "Berta", lastName: "Müller"};
+        const user3 = {firstName: "Claus", lastName: "Geiger"};
+        const user4 = {firstName: "Daniela", lastName: "Pfeiffer"};
+
+        return <div>
+            <UserList users={[user1, user2, user3, user4]}/>
+            <UserCreation/>
+        </div>;
+    }
 
 }
---
+++ code/src/User.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+
+export default class User extends Component {
+
+    render() {
+        return <li>
+            <span>{this.props.user.firstName} {this.props.user.lastName}</span>
+        </li>;
+    }
+}
